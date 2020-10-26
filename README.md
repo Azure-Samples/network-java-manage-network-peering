ee9b3482c05dbfb66e6b290cd3aa2b4fbd67148b
Azure-Fluent <azfluent@microsoft.com>
2020-10-26 13:11:59 +0800

    Release azure-resourcemanager 2.0.0

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ languages:
 products:
 - azure
 extensions:
-  services: virtual-network
+  services: Network
   platforms: java
 ---
 
@@ -13,48 +13,48 @@ extensions:
 
 
   Azure Network sample for enabling and updating network peering between two virtual networks
- 
+  <p>
   Summary ...
- 
+  <p>
   - This sample creates two virtual networks in the same subscription and then peers them, modifying various options on the peering.
- 
+  <p>
   Details ...
- 
+  <p>
   1. Create two virtual networks, network "A" and network "B"...
   - network A with two subnets
   - network B with one subnet
   - the networks' address spaces must not overlap
   - the networks must be in the same region
- 
+  <p>
   2. Peer the networks...
   - the peering will initially have default settings:
-    - each network's IP address spaces will be accessible from the other network
-    - no traffic forwarding will be enabled between the networks
-    - no gateway transit between one network and the other will be enabled
- 
+  - each network's IP address spaces will be accessible from the other network
+  - no traffic forwarding will be enabled between the networks
+  - no gateway transit between one network and the other will be enabled
+  <p>
   3. Update the peering...
   - disable IP address space between the networks
   - enable traffic forwarding from network A to network B
-  
+  <p>
   4. Delete the peering
   - the removal of the peering takes place on both networks, as long as they are in the same subscription
- 
-  Notes: 
+  <p>
+  Notes:
   - Once a peering is created, it cannot be pointed at another remote network later.
   - The address spaces of the peered networks cannot be changed as long as the networks are peered.
   - Gateway transit scenarios as well as peering networks in different subscriptions are possible but beyond the scope of this sample.
   - Network peering in reality results in pairs of peering objects: one pointing from one network to the other,
-    and the other peering object pointing the other way. For simplicity though, the SDK provides a unified way to
-    manage the peering as a whole, in a single command flow, without the need to duplicate commands for both sides of the peering,
-    while enforcing the required restrictions between the two peerings automatically, as this sample shows. But it is also possible
-    to modify each peering separately, which becomes required when working with networks in different subscriptions.
+  and the other peering object pointing the other way. For simplicity though, the SDK provides a unified way to
+  manage the peering as a whole, in a single command flow, without the need to duplicate commands for both sides of the peering,
+  while enforcing the required restrictions between the two peerings automatically, as this sample shows. But it is also possible
+  to modify each peering separately, which becomes required when working with networks in different subscriptions.
  
 
 ## Running this Sample ##
 
 To run this sample:
 
-Set the environment variable `AZURE_AUTH_LOCATION` with the full path for an auth file. See [how to create an auth file](https://github.com/Azure/azure-libraries-for-java/blob/master/AUTH.md).
+See [DefaultAzureCredential](https://github.com/Azure/azure-sdk-for-java/tree/master/sdk/identity/azure-identity#defaultazurecredential) and prepare the authentication works best for you. For more details on authentication, please refer to [AUTH.md](https://github.com/Azure/azure-sdk-for-java/blob/master/sdk/resourcemanager/docs/AUTH.md).
 
     git clone https://github.com/Azure-Samples/network-java-manage-network-peering.git
 
@@ -64,9 +64,11 @@ Set the environment variable `AZURE_AUTH_LOCATION` with the full path for an aut
 
 ## More information ##
 
-[http://azure.com/java](http://azure.com/java)
+For general documentation as well as quickstarts on how to use Azure Management Libraries for Java, please see [here](https://aka.ms/azsdk/java/mgmt).
 
-If you don't have a Microsoft Azure subscription you can get a FREE trial account [here](http://go.microsoft.com/fwlink/?LinkId=330212)
+Start to develop applications with Java on Azure [here](http://azure.com/java).
+
+If you don't have a Microsoft Azure subscription you can get a FREE trial account [here](http://go.microsoft.com/fwlink/?LinkId=330212).
 
 ---
 
--- src/main/java/com/azure/resourcemanager/network/samples/ManageNetworkPeeringInSameSubscription.java
+++ src/main/java/com/azure/resourcemanager/network/samples/ManageNetworkPeeringInSameSubscription.java
@@ -1,76 +1,76 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-package com.microsoft.azure.management.network.samples;
+package com.azure.resourcemanager.network.samples;
 
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.network.Network;
-import com.microsoft.azure.management.network.NetworkPeering;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.azure.management.resources.fluentcore.model.Creatable;
-import com.microsoft.azure.management.resources.fluentcore.model.CreatedResources;
-import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
-import com.microsoft.azure.management.samples.Utils;
-import com.microsoft.rest.LogLevel;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.network.models.Network;
+import com.azure.resourcemanager.network.models.NetworkPeering;
+import com.azure.core.management.Region;
+import com.azure.resourcemanager.resources.fluentcore.model.Creatable;
+import com.azure.resourcemanager.resources.fluentcore.model.CreatedResources;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.resourcemanager.samples.Utils;
 
-import java.io.File;
 import java.util.Arrays;
 
 /**
  * Azure Network sample for enabling and updating network peering between two virtual networks
- *
+ * <p>
  * Summary ...
- *
+ * <p>
  * - This sample creates two virtual networks in the same subscription and then peers them, modifying various options on the peering.
- *
+ * <p>
  * Details ...
- *
+ * <p>
  * 1. Create two virtual networks, network "A" and network "B"...
  * - network A with two subnets
  * - network B with one subnet
  * - the networks' address spaces must not overlap
  * - the networks must be in the same region
- *
+ * <p>
  * 2. Peer the networks...
  * - the peering will initially have default settings:
- *   - each network's IP address spaces will be accessible from the other network
- *   - no traffic forwarding will be enabled between the networks
- *   - no gateway transit between one network and the other will be enabled
- *
+ * - each network's IP address spaces will be accessible from the other network
+ * - no traffic forwarding will be enabled between the networks
+ * - no gateway transit between one network and the other will be enabled
+ * <p>
  * 3. Update the peering...
  * - disable IP address space between the networks
  * - enable traffic forwarding from network A to network B
- * 
+ * <p>
  * 4. Delete the peering
  * - the removal of the peering takes place on both networks, as long as they are in the same subscription
- 
- * Notes: 
+ * <p>
+ * Notes:
  * - Once a peering is created, it cannot be pointed at another remote network later.
  * - The address spaces of the peered networks cannot be changed as long as the networks are peered.
  * - Gateway transit scenarios as well as peering networks in different subscriptions are possible but beyond the scope of this sample.
  * - Network peering in reality results in pairs of peering objects: one pointing from one network to the other,
- *   and the other peering object pointing the other way. For simplicity though, the SDK provides a unified way to
- *   manage the peering as a whole, in a single command flow, without the need to duplicate commands for both sides of the peering,
- *   while enforcing the required restrictions between the two peerings automatically, as this sample shows. But it is also possible
- *   to modify each peering separately, which becomes required when working with networks in different subscriptions.
+ * and the other peering object pointing the other way. For simplicity though, the SDK provides a unified way to
+ * manage the peering as a whole, in a single command flow, without the need to duplicate commands for both sides of the peering,
+ * while enforcing the required restrictions between the two peerings automatically, as this sample shows. But it is also possible
+ * to modify each peering separately, which becomes required when working with networks in different subscriptions.
  */
 
 public final class ManageNetworkPeeringInSameSubscription {
 
     /**
      * Main function which runs the actual sample.
-     * @param azure instance of the azure client
+     *
+     * @param azureResourceManager instance of the azure client
      * @return true if sample runs successfully
      */
-    public static boolean runSample(Azure azure) {
+    public static boolean runSample(AzureResourceManager azureResourceManager) {
         final Region region = Region.US_EAST;
-        final String resourceGroupName = SdkContext.randomResourceName("rg", 15);
-        final String vnetAName = SdkContext.randomResourceName("net", 15);
-        final String vnetBName = SdkContext.randomResourceName("net", 15);
-        final String peeringABName = SdkContext.randomResourceName("peer", 15);
+        final String resourceGroupName = Utils.randomResourceName(azureResourceManager, "rg", 15);
+        final String vnetAName = Utils.randomResourceName(azureResourceManager, "net", 15);
+        final String vnetBName = Utils.randomResourceName(azureResourceManager, "net", 15);
+        final String peeringABName = Utils.randomResourceName(azureResourceManager, "peer", 15);
         try {
 
             //=============================================================
@@ -78,25 +78,25 @@ public final class ManageNetworkPeeringInSameSubscription {
 
             System.out.println("Creating two virtual networks in the same region and subscription...");
 
-            Creatable<Network> networkADefinition = azure.networks().define(vnetAName)
+            Creatable<Network> networkADefinition = azureResourceManager.networks().define(vnetAName)
                     .withRegion(region)
                     .withNewResourceGroup(resourceGroupName)
                     .withAddressSpace("10.0.0.0/27")
                     .withSubnet("subnet1", "10.0.0.0/28")
                     .withSubnet("subnet2", "10.0.0.16/28");
 
-            Creatable<Network> networkBDefinition = azure.networks().define(vnetBName)
+            Creatable<Network> networkBDefinition = azureResourceManager.networks().define(vnetBName)
                     .withRegion(region)
                     .withNewResourceGroup(resourceGroupName)
                     .withAddressSpace("10.1.0.0/27")
                     .withSubnet("subnet3", "10.1.0.0/27");
 
             // Create the networks in parallel for better performance
-            CreatedResources<Network> created = azure.networks().create(Arrays.asList(networkADefinition, networkBDefinition));
+            CreatedResources<Network> created = azureResourceManager.networks().create(Arrays.asList(networkADefinition, networkBDefinition));
 
             // Print virtual network details
             for (Network network : created.values()) {
-                Utils.print((Network) network);
+                Utils.print(network);
                 System.out.println();
             }
 
@@ -109,9 +109,9 @@ public final class ManageNetworkPeeringInSameSubscription {
 
             System.out.println(
                     "Peering the networks using default settings...\n"
-                    + "- Network access enabled\n"
-                    + "- Traffic forwarding disabled\n"
-                    + "- Gateway use (transit) by the peered network disabled");
+                            + "- Network access enabled\n"
+                            + "- Traffic forwarding disabled\n"
+                            + "- Gateway use (transit) by the peered network disabled");
 
             NetworkPeering peeringAB = networkA.peerings().define(peeringABName)
                     .withRemoteNetwork(networkB)
@@ -127,9 +127,9 @@ public final class ManageNetworkPeeringInSameSubscription {
 
             System.out.println("Updating the peering ...");
             peeringAB.update()
-                .withoutAccessFromEitherNetwork()
-                .withTrafficForwardingFromRemoteNetwork()
-                .apply();
+                    .withoutAccessFromEitherNetwork()
+                    .withTrafficForwardingFromRemoteNetwork()
+                    .apply();
 
             System.out.println("Updated the peering to disallow network access between B and A but allow traffic forwarding from B to A.");
 
@@ -150,26 +150,21 @@ public final class ManageNetworkPeeringInSameSubscription {
             Utils.print(networkB);
 
             return true;
-        } catch (Exception f) {
-
-            System.out.println(f.getMessage());
-            f.printStackTrace();
-
         } finally {
             try {
                 System.out.println("Deleting Resource Group: " + resourceGroupName);
-                azure.resourceGroups().beginDeleteByName(resourceGroupName);
+                azureResourceManager.resourceGroups().beginDeleteByName(resourceGroupName);
             } catch (NullPointerException npe) {
                 System.out.println("Did not create any resources in Azure. No clean up is necessary");
             } catch (Exception g) {
                 g.printStackTrace();
             }
         }
-        return false;
     }
 
     /**
      * Main entry point.
+     *
      * @param args parameters
      */
 
@@ -179,23 +174,27 @@ public final class ManageNetworkPeeringInSameSubscription {
             //=============================================================
             // Authenticate
 
-            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                .build();
 
-            Azure azure = Azure
-                    .configure()
-                    .withLogLevel(LogLevel.BODY.withPrettyJson(true))
-                    .authenticate(credFile)
-                    .withDefaultSubscription();
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                .configure()
+                .withLogLevel(HttpLogDetailLevel.BASIC)
+                .authenticate(credential, profile)
+                .withDefaultSubscription();
 
             // Print selected subscription
-            System.out.println("Selected subscription: " + azure.subscriptionId());
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
 
-            runSample(azure);
+            runSample(azureResourceManager);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
     }
+
     private ManageNetworkPeeringInSameSubscription() {
 
     }
